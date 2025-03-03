95bf798784e54bf52f9ae8910520b9b68eab091c
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2025-03-03 20:48:44 +0100

    prace na db

--- SemestralniPrace/Form1.Designer.cs
+++ SemestralniPrace/Form1.Designer.cs
@@ -31,20 +31,28 @@
             this.listView_pokrmy = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.listView_ingredience = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView_pokrmy
             // 
+            this.listView_pokrmy.FullRowSelect = true;
             this.listView_pokrmy.HideSelection = false;
             this.listView_pokrmy.Location = new System.Drawing.Point(19, 30);
+            this.listView_pokrmy.MultiSelect = false;
             this.listView_pokrmy.Name = "listView_pokrmy";
             this.listView_pokrmy.Size = new System.Drawing.Size(483, 246);
             this.listView_pokrmy.TabIndex = 0;
+            this.listView_pokrmy.TileSize = new System.Drawing.Size(483, 36);
             this.listView_pokrmy.UseCompatibleStateImageBehavior = false;
+            this.listView_pokrmy.View = System.Windows.Forms.View.Tile;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(611, 327);
+            this.button1.Location = new System.Drawing.Point(19, 394);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 44);
             this.button1.TabIndex = 1;
@@ -55,22 +63,69 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(582, 134);
+            this.label1.Location = new System.Drawing.Point(16, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(117, 16);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Jídla, co lze udělat";
+            // 
+            // listView_ingredience
+            // 
+            this.listView_ingredience.FullRowSelect = true;
+            this.listView_ingredience.HideSelection = false;
+            this.listView_ingredience.Location = new System.Drawing.Point(613, 30);
+            this.listView_ingredience.MultiSelect = false;
+            this.listView_ingredience.Name = "listView_ingredience";
+            this.listView_ingredience.ShowGroups = false;
+            this.listView_ingredience.Size = new System.Drawing.Size(175, 246);
+            this.listView_ingredience.TabIndex = 3;
+            this.listView_ingredience.UseCompatibleStateImageBehavior = false;
+            this.listView_ingredience.View = System.Windows.Forms.View.Tile;
+            this.listView_ingredience.DoubleClick += new System.EventHandler(this.listView_ingredience_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(613, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingredience ve skladu";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "přidat ingredience do skladu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 372);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView_ingredience);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView_pokrmy);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Kucharka";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +136,10 @@
         private System.Windows.Forms.ListView listView_pokrmy;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView_ingredience;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SemestralniPrace/Form1.cs
+++ SemestralniPrace/Form1.cs
@@ -1,28 +1,35 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SemestralniPrace
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
-            
+
             InitializeComponent();
+            AktualizujViews();
         }
+
+        //TODO metoda na zjišťování dat z jakékoliv tabulky - zatím je jen ze skladu
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ZjistiZDatabaze("select * from Sklad");
-
+            ZjistiZDatabaze("select * from Ingredience");
+            //ZjistiZDatabaze("select nazev_ingredience from Ingredience where id_ingredience = 1"); - nutnost parsovat jen string
 
 
             /*  odebrani z databaze
@@ -38,6 +45,7 @@ namespace SemestralniPrace
         /// <param name="prikaz">příkat, který chcete udělat nad databází</param>
         private void ZjistiZDatabaze(string prikaz)
         {
+
             //zjištění adresy, kde je uložen projekt a nalezení databáze
             string directory = Application.StartupPath;
             int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
@@ -54,9 +62,16 @@ namespace SemestralniPrace
                     var reader = com.ExecuteReader();
                     while (reader.Read())
                     {
-                        var id = reader.GetInt32(0);
-                        var nazev = reader.GetString(1);
-                        Console.WriteLine($"{id}, {nazev}");
+                        try
+                        {
+                            var id = reader.GetInt32(0);
+                            var nazev = reader.GetString(1);
+                            Console.WriteLine($"{id}, {nazev}");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
                     }
 
 
@@ -65,12 +80,9 @@ namespace SemestralniPrace
             }
         }
 
-        /// <summary>
-        /// Příkaz na vkládání/úpravy dat v databázi
-        /// </summary>
-        /// <param name="prikaz">příkat, který chcete udělat nad databází</param>
-        private void VlozNeboUpravDataZDatabaze(string prikaz)
+        private void ZjistiZVazebniTabulky(string prikaz)
         {
+
             //zjištění adresy, kde je uložen projekt a nalezení databáze
             string directory = Application.StartupPath;
             int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
@@ -84,11 +96,425 @@ namespace SemestralniPrace
                     com.CommandText = prikaz;
                     com.Connection = conn;
                     conn.Open();
-                    com.ExecuteNonQuery();
+                    var reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            var id1 = reader.GetInt32(0);
+                            var id2 = reader.GetInt32(1);
+                            var pocet = reader.GetInt32(2);
+                            Console.WriteLine($"ingredience: {id1} sklad: {id2}, {pocet} ks/g");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
+
+                    }
+
 
                     conn.Close();
                 }
             }
+        }
+
+        private List<string> ZjistiNazevAPocetKs(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+            using (SQLiteConnection conn = new SQLiteConnection("data source=" + directory))
+            {
+                using (SQLiteCommand com = new SQLiteCommand())
+                {
+                    com.CommandText = prikaz;
+                    com.Connection = conn;
+                    conn.Open();
+                    var reader = com.ExecuteReader();
+                    List<string> pole = new List<string>();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            var nazev = reader.GetString(0);
+                            var pocet = reader.GetInt32(1);
+                            //Console.WriteLine($"nazev: {nazev}, {pocet} ks/g");
+                            pole.Add($"{nazev} - {pocet} ks/g");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
+
+                    }
+
+
+                    conn.Close();
+
+                    if (pole.Count > 0)
+                    {
+                        return pole;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        private List<string> ZjistiNazev(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+            using (SQLiteConnection conn = new SQLiteConnection("data source=" + directory))
+            {
+                using (SQLiteCommand com = new SQLiteCommand())
+                {
+                    com.CommandText = prikaz;
+                    com.Connection = conn;
+                    conn.Open();
+                    var reader = com.ExecuteReader();
+                    List<string> pole = new List<string>();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            var nazev = reader.GetString(0);
+                            //Console.WriteLine($"nazev: {nazev}, {pocet} ks/g");
+                            pole.Add(nazev);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
+
+                    }
+
+
+                    conn.Close();
+
+                    if (pole.Count > 0)
+                    {
+                        return pole;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        private List<(int, string)> ZjistiIdANazev(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+            using (SQLiteConnection conn = new SQLiteConnection("data source=" + directory))
+            {
+                using (SQLiteCommand com = new SQLiteCommand())
+                {
+                    com.CommandText = prikaz;
+                    com.Connection = conn;
+                    conn.Open();
+                    var reader = com.ExecuteReader();
+                    List<(int,string)> pole = new List<(int,string)>();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            var id = reader.GetInt32(0);
+                            var nazev = reader.GetString(1);
+                            //Console.WriteLine($"nazev: {nazev}, {pocet} ks/g
+                            pole.Add((id, nazev));
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
+
+                    }
+
+
+                    conn.Close();
+
+                    if (pole.Count > 0)
+                    {
+                        return pole;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        private List<int> ZjistiCislo(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+            using (SQLiteConnection conn = new SQLiteConnection("data source=" + directory))
+            {
+                using (SQLiteCommand com = new SQLiteCommand())
+                {
+                    com.CommandText = prikaz;
+                    com.Connection = conn;
+                    conn.Open();
+                    var reader = com.ExecuteReader();
+                    List<int> pole = new List<int>();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            int cislo = reader.GetInt32(0);
+                            pole.Add(cislo);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Chyba");
+                        }
+
+                    }
+
+
+                    conn.Close();
+
+                    if (pole.Count > 0)
+                    {
+                        return pole;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        //HÁZELO CHYBU DATABASE IS LOCKED PŘI NÁSLEDNÉM POKUSU O ZMĚNU(UPDATE) DAT
+        private bool Existuje(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+            using (SQLiteConnection conn = new SQLiteConnection("data source=" + directory))
+            {
+                using (SQLiteCommand com = new SQLiteCommand())
+                {
+                    try
+                    {
+                        com.CommandText = prikaz;
+                        com.Connection = conn;
+                        conn.Open();
+                        var reader = com.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            conn.Close();
+                            return true;
+                        }
+
+                        conn.Close();
+                        return false;
+                    }
+                    catch (Exception)
+                    {
+                        conn.Close();
+                        Console.WriteLine("Chyba");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Příkaz na vkládání/úpravy dat v databázi
+        /// </summary>
+        /// <param name="prikaz">příkat, který chcete udělat nad databází</param>
+        /// <returns>true/false, zda se vše vykonalo v pořádku</returns>
+        private bool VlozNeboUpravDataZDatabaze(string prikaz)
+        {
+
+            //zjištění adresy, kde je uložen projekt a nalezení databáze
+            string directory = Application.StartupPath;
+            int odebrany = directory.IndexOf("\\SemestralniPrace\\bin\\Debug");
+            directory = directory.Substring(0, odebrany) + "\\databaze.sqlite";
+            //Console.WriteLine(directory);
+
+            using (SQLiteConnection conn = new SQLiteConnection($"data source= {directory}"))
+            {
+                conn.Open();
+
+                using (var transaction = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        using (SQLiteCommand com = new SQLiteCommand())
+                        {
+                            com.Transaction = transaction;
+                            com.CommandText = prikaz;
+                            com.Connection = conn;
+                            com.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception x)
+                    {
+                        transaction.Rollback();
+                    Interaction.MsgBox(x.Message);
+                        return false;
+                    }
+                    conn.Close();
+                }
+
+            }
+
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TODO vyskočí okno, které se zeptá co a kolik toho chce přidat
+            //(jestli existuje v databázi, tak se jen přidá počet - buď se vytvoří relace do
+            //IngredienceVeSkladu, nebo přidá do tabulky počet. jestli ne, tak se nejdřív zeptá,
+            //jestli je název správně a pak vytvoří nový prvek v Ingredienci a )
+
+            try
+            {
+                string nazev = Interaction.InputBox("zadej nazev", "nazev");
+                nazev = nazev.Trim();
+                //Console.WriteLine(nazev);
+                int pocet = int.Parse(Interaction.InputBox("zadej pocet", "pocet"));
+                //Console.WriteLine(pocet);
+
+                //zjistí id dané ingredience (pokud existuje)
+                List<int> list = ZjistiCislo($"select i.id_ingredience from Ingredience i where nazev_ingredience = \'{nazev}\'");
+                if (list == null)
+                {
+                    //vloží ingredienci do tabulky Ingredience
+                    VlozNeboUpravDataZDatabaze($"INSERT INTO Ingredience VALUES (null ,\'{nazev}\')");
+                    //vloží id ingredience - nyní už 100% existuje
+                    list = ZjistiCislo($"select i.id_ingredience from Ingredience i where nazev_ingredience = \'{nazev}\'");
+
+                }
+                int ingredience = list[0];
+                //zkontrolovat, jestli je ve vazební tabulce a pak buď přidat, nebo změnit
+                List<int> existuje = ZjistiCislo($"select i.id_skladu from IngredienceVeSkladu i where i.id_ingredience = {ingredience}");
+                if (existuje != null)
+                {
+                    //updatne ingredienci na danou hodnotu
+                    //Interaction.MsgBox("ingredience existuje");
+                    VlozNeboUpravDataZDatabaze($"UPDATE IngredienceVeSkladu SET pocet = {pocet} WHERE id_ingredience = {ingredience} and id_skladu = 1");
+                }
+                else
+                {
+                    //vloží novou ingredienci do vazební tabulky IngredienceVeSKladu
+                    VlozNeboUpravDataZDatabaze($"INSERT INTO IngredienceVeSkladu VALUES ({ingredience}, 1 ,{pocet})");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Chyba při zadávání");
+            }
+
+            AktualizujViews();
+        }
+
+        private List<string> ZjistiNazevIngredienceAPocetZeSkladu1()
+        {
+            return ZjistiNazevAPocetKs("select * from ViewNazevAPocetIngVeSkladu1");
+            //return ZjistiNazevAPocetKs("select ing.nazev_ingredience, " +
+            //"i.pocet from IngredienceVeSkladu i left join Ingredience ing " +
+            //"where i.id_ingredience = ing.id_ingredience and i.id_skladu = 1");
+        }
+        private void AktualizujListViewIngredience()
+        {
+            List<string> list = ZjistiNazevIngredienceAPocetZeSkladu1();
+            listView_ingredience.Clear();
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    listView_ingredience.Items.Add(new ListViewItem((string)item));
+                }
+            }
+        }
+        private List<string> ZjistiNazevMoznehoJidla()
+        {
+            return ZjistiNazev("select distinct(j.nazev) from Jidlo j left join IngredienceVJidle ivj using (id_jidla) " +
+                "left join(select * from IngredienceVeSkladu where id_skladu = 1) ivs using (id_ingredience) " +
+                "except " +
+                "select j.nazev from Jidlo j left join IngredienceVJidle ivj using (id_jidla) " +
+                "left join " +
+                "(select* from IngredienceVeSkladu where id_skladu = 1) ivs " +
+                "using (id_ingredience) " +
+                "where ivs.pocet < ivj.pocet; ");
+        }
+
+        private void AktualizujListViewPokrmy()
+        {
+            List<string> list = ZjistiNazevMoznehoJidla();
+            listView_pokrmy.Clear();
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    listView_pokrmy.Items.Add(new ListViewItem(item));
+                }
+            }
+        }
+
+        private void AktualizujViews()
+        {
+            AktualizujListViewIngredience();
+            AktualizujListViewPokrmy();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void listView_ingredience_DoubleClick(object sender, EventArgs e)
+        {
+            string nazev = zjistiNazevVybraneIngredience();
+
+            if (nazev == null) {
+                Interaction.MsgBox("Nevybrána žádná ingredience");
+            }
+
+            //TODO vyskakovací okno s předvyplněnýma hodnotama pro úpravu počtu kusů
+            //možná i pro změnu názvu
+            //Interaction.MsgBox(nazev);
+            List<(int, string)> list = ZjistiIdANazev($"select i.nazev_ingredience, " +
+                $"ivs.pocet from IngredienceVeSkladu ivs left join Ingredience i using " +
+                $"(id_ingredience) where i.nazev_ingredience = '{nazev}';");
+
+            if (list != null)
+            {
+                Console.Write(list[0].Item1);
+            }
+        }
+
+        private string zjistiNazevVybraneIngredience()
+        {
+            var items = listView_ingredience.SelectedItems;
+            if (items != null)
+            {
+                int index = items[0].Text.IndexOf('-');
+                string nazev = items[0].Text.Substring(0, index);
+                Console.WriteLine(nazev);
+                return nazev;
+            }
+            return null;
         }
     }
 }
