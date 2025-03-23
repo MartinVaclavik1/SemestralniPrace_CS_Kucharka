47ffb738621506aef6fe86e8d938ef4da94242e6
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2025-03-23 23:53:32 +0100

    změna názvů forms

--- SemestralniPrace/FormUpravaIngredience.Designer.cs
+++ SemestralniPrace/FormUpravaIngredience.Designer.cs
@@ -61,7 +61,7 @@
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(85, 109);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            10000,
+            100000,
             0,
             0,
             0});
